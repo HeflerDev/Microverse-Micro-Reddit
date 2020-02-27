1b13a4ddaa6adf0bac798a1c8ced0827c0557ef5
Kasuhira <hheflerdevelopment@gmail.com>
2020-02-27 14:55:29 -0300

    Added Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_163352) do
+ActiveRecord::Schema.define(version: 2020_02_27_175345) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_02_26_163352) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
